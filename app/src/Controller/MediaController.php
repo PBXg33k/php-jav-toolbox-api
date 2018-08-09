bff1fe71adc8b895c7c2b22c8ca7cd05cdf0361c
PBX_g33k <development@oguzhanuysal.eu>
2018-08-10 00:08:57 +0200

    add endpoint to extract catalogID using url

--- app/src/Controller/MediaController.php
+++ app/src/Controller/MediaController.php
@@ -4,12 +4,24 @@ namespace App\Controller;
 
 use App\Entity\Title;
 use App\Repository\TitleRepository;
+use App\Service\JAVProcessorService;
 use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MediaController extends Controller
 {
+    /**
+     * @var JAVProcessorService
+     */
+    private $JAVProcessorService;
+
+    public function __construct(JAVProcessorService $JAVProcessorService)
+    {
+        $this->JAVProcessorService = $JAVProcessorService;
+    }
+
     /**
      * @Route("/media", name="media")
      */
@@ -20,5 +32,19 @@ class MediaController extends Controller
         return $this->json([
             'titles' => $titles
         ]);
+    }
+
+    /**
+     * @Route("/media/filename/{filenameSlug}")
+     */
+    public function getIDFromFilename(Request $request, string $filenameSlug)
+    {
+        try {
+            $javFile = JAVProcessorService::extractIDFromFilename($filenameSlug);
+
+            return $this->json($javFile);
+        } catch (\Exception $e) {
+            throw $e;
+        }
     }
 }
