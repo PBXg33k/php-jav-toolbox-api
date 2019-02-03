a43e4dd7b8488583c1caf59e652b27c6bf246ec9
PBX_g33k <development@oguzhanuysal.eu>
2019-02-03 21:29:28 +0100

    dependency updates broke some shit

--- app/src/Migrations/Version20180421162228.php
+++ app/src/Migrations/Version20180421162228.php
@@ -2,7 +2,7 @@
 
 namespace DoctrineMigrations;
 
-use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
@@ -10,7 +10,7 @@ use Doctrine\DBAL\Schema\Schema;
  */
 class Version20180421162228 extends AbstractMigration
 {
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
@@ -35,7 +35,7 @@ class Version20180421162228 extends AbstractMigration
         $this->addSql('ALTER TABLE title_model ADD CONSTRAINT FK_1764C4AE7975B7E7 FOREIGN KEY (model_id) REFERENCES model (id) ON DELETE CASCADE');
     }
 
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
